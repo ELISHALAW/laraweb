7603a7e2b42bf9f2592c9c6a8ef95bec4aa2ae2f
ELISHALAW <seongchunlaw050@gmail.com>
2026-02-21 16:35:37 +0800

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
+
+Route::get('contact',function(){
+    return view('contact');
+})->name('contact');
