d27d4b53ed1b8e694f6e38a6da7198a2dab3a1a4
ELISHALAW <seongchunlaw050@gmail.com>
2026-02-22 22:46:36 +0800

    law20260222 Adding company, organization and portfolio

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -3,11 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Us 🤣</title>
-    @vite('resources/css/app.css')
-    @vite('resources/js/app.js')
+    <title>Portfolio website 😒</title>
 </head>
 <body>
-    <h1>Contact Us</h1>
+    <h1>Portfolio Page</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,35 @@
 
 use Illuminate\Support\Facades\Route;
 
-
+// Get route example
 Route::get('/home', function () {
     return view('home');
 })->name('home');
 
-Route::get('contact',function(){
-    return view('contact');
-})->name('contact');
+
+// Grouped routes Portfolio related routes
+Route::get('/portfolio', function(){
+    return view('portfolio');
+});
+// prefix is to tell the browser that all the routes inside the group will have the same prefix which is portfolio in this case, so we don't have to write it again and again for each route
+Route::prefix("portfolio")->group(function(){
+
+    Route::get('/company', function(){
+    return view('company');
+    });
+
+    Route::get('/organization', function(){
+        return view('organization');
+    });
+});
+
+
+
+// Parameter using routes
+// Route::get('/portfolio/{firstname}/{lastname}', function ($firstname, $lastname) {
+//     return $firstname . " " . $lastname;
+// });
+
+// Route::get('/test', function () {
+//     return "This is a test";
+// })->name('testpage');
