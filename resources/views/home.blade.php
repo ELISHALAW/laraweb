9c48ac56e493c50fda4b9ab7ba495511cd5bb15d
ELISHALAW <seongchunlaw050@gmail.com>
2026-02-22 23:42:12 +0800

    Law20260222 Adding Route::post to the laravel website

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home 😂</title>
-    @vite('resources/css/app.css')
-    @vite('resources/js/app.js')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
 </head>
 <body>
     <h1>Home</h1>
+
+    <form action="{{ route('formsubmitted') }}" method="POST">
+    @csrf
+    <label for="Fullname">Full name:</label>
+        <input type="text" id="fullname" name="fullname" placeholder="Type your full name here" required>
+        <br>
+        <br>
+    <label for="Email">Email:</label>
+        <input type="text" id="email" name="email" placeholder="Type your email here" required><br>
+        <br><br>
+        <button type="submit">Submit</button>
+    </form>
     {{-- <a href="{{ route("testpage") }}">Go to test page</a> --}}
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 // Get route example
 Route::get('/home', function () {
@@ -9,20 +10,32 @@ Route::get('/home', function () {
 
 
 // Grouped routes Portfolio related routes
-Route::get('/portfolio', function(){
+Route::get('/portfolio', function () {
     return view('portfolio');
 });
 // prefix is to tell the browser that all the routes inside the group will have the same prefix which is portfolio in this case, so we don't have to write it again and again for each route
-Route::prefix("portfolio")->group(function(){
+Route::prefix("portfolio")->group(function () {
 
-    Route::get('/company', function(){
-    return view('company');
+    Route::get('/company', function () {
+        return view('company');
     });
 
-    Route::get('/organization', function(){
+    Route::get('/organization', function () {
         return view('organization');
     });
 });
+
+Route::post("/formsubmitted", function (Request $request) {
+    $request->validate([
+        "fullname" => "required | min:3| max:30",
+        "email" => "required|min:3|max:30|email"
+    ]);
+
+    $fullname = $request->input("fullname");
+    $email = $request->input("email");
+
+    return "Your full name is {$request->input('fullname')} and your email is {$request->input('email')}";
+})->name("formsubmitted");
 
 
 
